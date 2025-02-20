d7469e383350410c8fc48835c348c36d0503fc8e
Christian <christianbryan.alperto@aei.com>
2025-02-20 11:42:57 +0800

    Initial upload

---
+++ RobotSimulator/Program.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RobotSimulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RobotBaseClass robot = new RobotBaseClass();
+            bool isPlaceCommandExeuted = false;
+            Console.WriteLine("Instructions Below.");
+            Console.WriteLine("");
+            Console.WriteLine("Type the following commands.");
+            Console.WriteLine("1. Type 'Exit' to end the application");
+            Console.WriteLine("2. Type 'PLACE X,Y,F'");
+            Console.WriteLine("     - PLACE will put the toy robot on the table in position X,Y and facing NORTH, SOUTH, EAST or WEST");
+            Console.WriteLine("3. Type 'Move'");
+            Console.WriteLine("     - This will move the toy robot one unit forward in the direction it is currently facing.");
+            Console.WriteLine("4. Type 'Left or 'Right'");
+            Console.WriteLine("     - This will rotate the robot 90 degrees in the specified direction without changing the position of the robot");
+            Console.WriteLine("5. Type 'Report'");
+            Console.WriteLine("     - This will announce the X,Y and F of the robot.");
+            Console.WriteLine("");
+            Console.WriteLine("IMPORTANT!!!");
+            Console.WriteLine("- A 'PLACE' command must be executed first!");
+           
+            do
+            {
+                string command = Console.ReadLine().ToUpper();
+                if (command.ToUpper() == "EXIT") break;
+
+                /*Valid command type*/
+                if (command.Length >= 4)
+                {
+                    string commandValue = command.Substring(0, 4).ToUpper();
+                    if (commandValue != "PLAC" && !isPlaceCommandExeuted)
+                    {
+                        ShowInvalidText();
+                        Console.WriteLine("'PLACE' command must be executed first!");
+                        robot.LogCommand("PLACE", "Not yet set first");
+                    }
+
+                    switch (commandValue)
+                    {
+                        case "PLAC":
+                            isPlaceCommandExeuted = true;
+                            string[] parameters = command.Split(',');
+                            if (parameters.Count() == 3)
+                            {
+                                /*Get the x value*/
+                                string[] xparam = parameters[0].Split(' ');
+                                if (xparam.Count() == 0)
+                                {
+                                    ShowInvalidText();
+                                    robot.LogCommand("PLACE", "Invalid command");
+                                    break;
+                                }
+
+                                /*Check if the x and y are integers*/
+                                if (!int.TryParse(xparam[1], out int xvalue))
+                                {
+                                    ShowInvalidText();
+                                    robot.LogCommand("PLACE", "Invalid command");
+                                    break;
+                                }
+                                if (!int.TryParse(parameters[1], out int yvalue))
+                                {
+                                    ShowInvalidText();
+                                    robot.LogCommand("PLACE", "Invalid command");
+                                    break;
+                                }
+
+                                string direc = CheckDirectionValue(parameters[2]);
+                                if (!string.IsNullOrEmpty(direc))
+                                {
+                                    robot.Place(xvalue, yvalue, direc);
+                                    robot.LogCommand("PLACE", "");
+                                }
+                                else
+                                {
+                                    ShowInvalidText();
+                                    robot.LogCommand("PLACE", "Invalid command");
+                                }
+                            }
+                            else
+                            {
+                                ShowInvalidText();
+                            }
+                                break;
+                        case "MOVE":
+                            robot.Move();
+                            robot.LogCommand("MOVE", "");
+                            break;
+                        case "LEFT":
+                            robot.MoveLeft();
+                            robot.LogCommand("LEFT", "");
+                            break;
+                        case "RIGH":
+                            robot.MoveRight();
+                            robot.LogCommand("RIGHT", "");
+                            break;
+                        case "REPO":
+                            robot.Report();
+                            robot.LogCommand("REPORT", "");
+                            break;
+                        default:
+                            ShowInvalidText();
+                            break;
+                    }
+                }
+                else
+                {
+                    ShowInvalidText();
+                    robot.LogCommand("INVALID", "Invalid command");
+                }
+            } while (true);
+        }
+
+        public static void ShowInvalidText()
+        {
+            Console.WriteLine("Invalid command!!!");
+        }
+
+        public static string CheckDirectionValue(string direction)
+        {
+            string returnValue = "";
+            switch (direction)
+            {
+                case "NORTH":
+                    returnValue = direction;
+                    break;
+                case "WEST":
+                    returnValue = direction;
+                    break;
+                case "SOUTH":
+                    returnValue = direction;
+                    break;
+                case "EAST":
+                    returnValue = direction;
+                    break;
+            }
+            return returnValue;
+        }
+    }
+}
---
+++ RobotSimulator/RobotBaseClass.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace RobotSimulator
+{
+    public class RobotBaseClass
+    {
+        int facingValue = 0;
+        int yMaxValue = 5;
+        int yMinValue = 0;
+        int xMaxValue = 5;
+        int xMinValue = 0;
+        public RobotBaseClass()
+        {
+            faceDirection = "NORTH";
+            facingValue = 0;
+        }
+
+        public void MoveLeft()
+        {
+            if (facingValue == 0) facingValue = 360;
+            facingValue -= 90;
+            SetfaceCardinalDirection(facingValue);
+        }
+        public void MoveRight()
+        {
+            if (facingValue == 360) facingValue = 0;
+            facingValue += 90;
+            SetfaceCardinalDirection(facingValue);
+        }  
+
+        public void Move()
+        {
+            switch (faceDirection)
+            {
+                case "NORTH":
+                    if (Y + 1 <= yMaxValue)
+                    {
+                        Y++;
+                    }
+                    break;
+                case "WEST":
+                    if (x -1  >= xMinValue)
+                    {
+                        x--;
+                    }
+                    break;
+                case "SOUTH":
+                    if (y - 1 >= yMinValue)
+                    {
+                        y--;
+                    }
+                    break;
+                case "EAST":
+                    if (x + 1 >= xMaxValue)
+                    {
+                        x++;
+                    }
+                    break;
+            }
+        }
+
+        public void Place(int xpos,int ypos,string direction)
+        {
+            try
+            {
+                /*Do not allow negative value*/
+                /*Do not allow the robot to fall out of the table*/
+                if (xpos < xMinValue || ypos < yMinValue) return;
+                if (xpos > xMaxValue || ypos > yMaxValue) return;
+
+                
+                x = xpos;
+                y = ypos;
+                faceDirection = direction;
+
+                switch (faceDirection)
+                {
+                    case "NORTH":
+                        facingValue = 0;
+                        break;
+                    case "WEST":
+                        facingValue = 270;
+                        break;
+                    case "SOUTH":
+                        facingValue = 180;
+                        break;
+                    case "EAST":
+                        facingValue = 90;
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw;
+            }
+        }
+
+        public void Report()
+        {
+            Console.WriteLine(string.Format("{0},{1},{2}", x, y, faceDirection));
+        }
+        
+        private void SetfaceCardinalDirection(int degree)
+        {
+            string[] caridnals = { "N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE", "S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW", "N" };
+            var facing = caridnals[(int)Math.Round(((double)degree * 10 % 3600) / 225)];
+            switch (facing)
+            {
+                case "N":
+                    faceDirection = "NORTH";
+                    break;
+                case "W":
+                    faceDirection = "WEST";
+                    break;
+                case "S":
+                    faceDirection = "SOUTH";
+                    break;
+                case "E":
+                    faceDirection = "EAST";
+                    break;
+            }
+        }
+
+      
+        private string faceDirection;
+
+        public string FaceDirection 
+        {
+            get { return faceDirection; }
+            set { faceDirection = value; }
+        }
+
+        private int x;
+
+        public int  X
+        {
+            get { return x; }
+            set { x = value; }
+        }
+
+        private int y;
+
+        public int Y
+        {
+            get { return y; }
+            set { y = value; }
+        }
+
+        public  void LogCommand(string command,string message)
+        {
+            string position = string.Format("{0},{1},{2}", x, y, faceDirection);
+            using (var fileStream = new FileStream("commands.txt", FileMode.Append))
+            using (var streamWriter = new StreamWriter(fileStream))
+            {
+                streamWriter.WriteLine(DateTime.Now + "," + command +"," + position +"," + message);
+            }
+        }
+    }
+}
